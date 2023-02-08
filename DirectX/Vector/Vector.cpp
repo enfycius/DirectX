1337e02a1182fb0c63be0f5cfd373a159586bb97
enfycius <hexk0131@gmail.com>
2023-02-09 01:07:25 +0900

    Vector Algebra

---
+++ DirectX/Vector/Vector.cpp
@@ -0,0 +1,47 @@
+﻿// Vector Algebra.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <windows.h>
+#include <DirectXMath.h>
+#include <DirectXPackedVector.h>
+
+#include <iostream>
+
+using namespace std;
+using namespace DirectX;
+using namespace DirectX::PackedVector;
+
+ostream& XM_CALLCONV operator<<(ostream& os, FXMVECTOR v)
+{
+    XMFLOAT3 dest;
+    XMStoreFloat3(&dest, v);
+
+    os << "(" << dest.x << ", " << dest.y << ", " << dest.z << ")";
+
+    return os;
+}
+
+int main()
+{
+    cout.setf(ios_base::boolalpha);
+
+    if (!XMVerifyCPUSupport())
+    {
+        cout << "directx math not supported" << endl;
+        return 0;
+    }
+
+    XMVECTOR p = XMVectorZero();
+    XMVECTOR q = XMVectorSplatOne();
+    XMVECTOR u = XMVectorSet(1.0f, 2.0f, 3.0f, 0.0f);
+    XMVECTOR v = XMVectorReplicate(-2.0f);
+    XMVECTOR w = XMVectorSplatZ(u);
+
+    cout << "p = " << p << endl;
+    cout << "q = " << q << endl;
+    cout << "u = " << u << endl;
+    cout << "v = " << v << endl;
+    cout << "w = " << w << endl;
+
+    return 0;
+}
