60a9ae518bf05d1243a5a50f77d23517b6fe99f0
enfycius <hexk0131@gmail.com>
2023-02-09 11:03:14 +0900

    Matrix Algebra

---
+++ DirectX/Matrix/Matrix.cpp
@@ -0,0 +1,70 @@
+﻿// Matrix.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <Windows.h>
+#include <DirectXMath.h>
+#include <DirectXPackedVector.h>
+
+#include <iostream>
+
+using namespace std;
+using namespace DirectX;
+using namespace DirectX::PackedVector;
+
+ostream& XM_CALLCONV operator << (ostream& os, FXMVECTOR v)
+{
+    XMFLOAT4 dest;
+    XMStoreFloat4(&dest, v);
+
+    os << "(" << dest.x << ", " << dest.y << ", " << dest.z << ", " << dest.w << ")";
+
+    return os;
+}
+
+ostream& XM_CALLCONV operator << (ostream& os, FXMMATRIX m)
+{
+    for(int i=0; i<4; ++i)
+    {
+        os << XMVectorGetX(m.r[i]) << "\t";
+        os << XMVectorGetY(m.r[i]) << "\t";
+        os << XMVectorGetZ(m.r[i]) << "\t";
+        os << XMVectorGetW(m.r[i]);
+
+        os << endl;
+    }
+
+    return os;
+}
+
+int main(void)
+{
+    if (!XMVerifyCPUSupport())
+    {
+        cout << "Directx math not supported" << endl;
+
+        return 0;
+    }
+
+    XMMATRIX A(1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 2.0f, 0.0f, 0.0f, 0.0f, 0.0f, 4.0f, 0.0f, 1.0f, 2.0f, 3.0f, 1.0f);
+
+    XMMATRIX B = XMMatrixIdentity();
+
+    XMMATRIX C = A * B;
+
+    XMMATRIX D = XMMatrixTranspose(A);
+
+    XMVECTOR det = XMMatrixDeterminant(A);
+    XMMATRIX E = XMMatrixInverse(&det, A);
+
+    XMMATRIX F = A * E;
+
+    cout << "A = " << endl << A << endl;
+    cout << "B = " << endl << B << endl;
+    cout << "C = A*B = " << endl << C << endl;
+    cout << "D = tranpose(A) = " << endl << D << endl;
+    cout << "det = determinant(A) = " << det << endl << endl;
+    cout << "E = inverse(A) = " << endl << E << endl;
+    cout << "F = A*E = " << endl << F << endl;
+
+    return 0;
+}
