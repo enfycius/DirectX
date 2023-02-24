e1060f1cdaa566652421f04dc86c70776cf464a5
enfycius <hexk0131@gmail.com>
2023-02-25 00:32:40 +0900

    Update Initialization

---
+++ DirectX/Init/Init/Init.cpp
@@ -0,0 +1,2 @@
+﻿#include "./d3dApp.h"
+
---
+++ DirectX/Init/Init/d3dApp.cpp
@@ -0,0 +1,217 @@
+#include "d3dApp.h"
+#include <WindowsX.h>
+
+using Microsoft::WRL::ComPtr;
+using namespace std;
+using namespace DirectX;
+
+LRESULT CALLBACK
+
+MainWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    return D3DApp::GetApp()->MsgProc(hwnd, msg, wParam, lParam);
+}
+
+D3DApp* D3DApp::mApp = nullptr;
+
+D3DApp* D3DApp::GetApp()
+{
+    return mApp;
+}
+
+D3DApp::D3DApp(HINSTANCE hInstance) : mhAppInst(hInstance)
+{
+    assert(mApp == nullptr);
+    mApp = this;
+}
+
+D3DApp::~D3DApp()
+{
+    if(md3dDevice != nullptr)
+        FlushCommandQueue();
+}
+
+HINSTANCE D3DApp::AppInst() const
+{
+    return mhAppInst;
+}
+
+HWND D3DApp::MainWnd() const
+{
+    return mhMainWnd;
+}
+
+float D3DApp::AspectRatio() const
+{
+    return static_cast<float>(mClientWidth) / mClientHeight;
+}
+
+bool D3DApp::Get4xMsaaState() const
+{
+    return m4xMsaaState;
+}
+
+void D3DApp::Set4xMsaaState(bool value)
+{
+    if(m4xMsaaState != value)
+    {
+        m4xMsaaState = value;
+
+        CreateSwapChain();
+        OnResize();
+    }
+}
+
+int D3DApp::Run()
+{
+    MSG msg = {0};
+
+    mTimer.Reset();
+
+    while(msg.message != WM_QUIT)
+    {
+        if(PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+        else
+        {
+            mTimer.Tick();
+
+            if(!mAppPaused)
+            {
+                CalculateFrameStats();
+                Update(mTimer);
+                Draw(mTimer);
+            }
+            else
+            {
+                Sleep(100);
+            }
+        }
+    }
+
+    return (int)msg.wParam;
+}
+
+bool D3DApp::Initialize()
+{
+    if(!InitMainWindow())
+        return false;
+    
+    if(!InitDirect3D())
+        return false;
+
+    OnResize();
+
+    return true;
+}
+
+void D3DApp::CreateRtvAndDsvDescriptorHeaps()
+{
+    D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc;
+    rtvHeapDesc.NumDescriptors = SwapChainBufferCount;
+    rtvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+    rtvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+    rtvHeapDesc.NodeMask = 0;
+
+    ThrowIfFailed(md3dDevice->CreateDescriptorHeap(
+        &rtvHeapDesc, IID_PPV_ARGS(mRtvHeap.GetAddressOf())));
+
+    D3D12_DESCRIPTOR_HEAP_DESC dsvHeapDesc;
+    dsvHeapDesc.NumDescriptors = 1;
+    dsvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_DSV;
+    dsvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+    dsvHeapDesc.NodeMask = 0;
+
+    ThrowIfFailed(md3dDevice->CreateDescriptorHeap(
+        &dsvHeapDesc, IID_PPV_ARGS(mDsvHeap.GetAddressOf())));
+}
+
+void D3DApp::OnResize()
+{
+    assert(md3dDevice);
+    assert(mSwapChain);
+    assert(mDirectCmdListAlloc);
+
+    FlushCommandQueue();
+
+    ThrowIfFailed(mCommandList->Reset(mDirectCmdListAlloc.Get(), nullptr));
+
+    for(int i=0; i<SwapChainBufferCount; ++i)
+        mSwapChainBuffer[i].Reset();
+
+    mDepthStencilBuffer.Reset();
+
+    ThrowIfFailed(mSwapChain->ResizeBuffers(
+        SwapChainBufferCount,
+        mClientWidth, mClientHeight,
+        mBackBufferFormat,
+        DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH));
+
+    mCurrBackBuffer = 0;
+
+    CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHeapHandle(mRtvHeap->GetCPUDescriptorHandleForHeapStart());
+
+    for(UINT i=0; i<SwapCHainBufferCount; i++)
+    {
+        ThrowIfFailed(mSwapChain->GetBuffer(i, IID_PPV_ARGS(&mSwapChainBuffer[i])));
+        md3dDevice->CreateRenderTargetView(mSwapChainBuffer[i].Get(), nullptr, rtvHeapHandle);
+        rtvHeapHandle.Offset(1, mRtvDesciptorSize);
+    }
+
+    D3D12_RESOUCE_DESC depthStencilDesc;
+    depthStencilDesc.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE2D;
+    depthStencilDesc.Alignment = 0;
+    depthStencilDesc.Width = mClientWidth;
+    depthStencilDesc.Height = mClientHeight;
+    depthStencilDesc.DepthOrArraySize = 1;
+    depthStencilDesc.MipLevels = 1;
+
+    depthStencilDesc.Format = DXGI_FORMAT_R24G8_TYPELESS;
+
+    depthStencilDesc.SampleDesc.Count = m4xMsaaState ? 4 : 1;
+    depthStencilDesc.SampleDesc.Quality = m4xMsaaState ? (m4xMsaaQuality - 1) : 0;
+    depthStencilDesc.Layout = D3D12_TEXTURE_LAYOUT_UNKNONW;
+    depthStencilDesc.Flags = D3D12_RESOURCE_FLAG_ALLOW_DEPTH_STENCIL;
+
+    D3D12_CLEAR_VALUE optClear;
+    optClear.Format = mDepthStencilFormat;
+    optClear.DepthStencil.Depth = 1.0f;
+    optClear.DepthStencil.Stencil = 0;
+
+    ThrowIfFailed(md3dDevice->CreateCommittedResource(
+        &CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT),
+        D3D12_HEAP_FLAG_NONE,
+        &depthStencilDesc,
+        D3D12_RESOURCE_STATE_COMMON,
+        &optClear,
+        IID_PPV_ARGS(mDepthStencilBuffer.GetAddressOf())));
+
+    
+    D3D12_DEPTH_STENCIL_VIEW_DESC dsvDesc;
+    dsvDesc.Flags = D3D12_DSV_FLAG_NONE;
+    dsvDesc.ViewDimension = D3D12_DSV_DIMENSION_TEXTURE2D;
+    dsvDesc.Format = mDepthStencilFormat;
+    dsvDesc.Texture2D.MipSlice = 0;
+    md3dDevice->CreateDepthStencilView(mDepthStencilBuffer.Get(), &dsvDesc, DepthStencilView());
+
+    mCommandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(mDepthStencilBuffer.Get(),
+        D3D12_RESOURCE_STATE_COMMON, D3D12_RESOURCE_STATE_DEPTH_WRITE));
+
+    ThrowIfFailed(mCommandList->Close());
+    ID3D12CommandList* cmdsLists[] = { mCommandList.Get() };
+    mCommandQueue->ExecuteCommandLists(_countof(cmdsLists), cmdsLists);
+
+    FlushCommandQueue();
+
+    mScreenViewport.TopLeftX = 0;
+    mScreenViewport.TopLeftY = 0;
+    mScreenViewport.Width = static_cast<float>(mClientWidth);
+    mScreenViewport.Height = static_cast<float>(mClientHeight);
+    mScreenViewport.MinDepth = 0.0f;
+    mScreenViewport.MaxDepth = 1.0f;
+
+    mScissorRect = {0, 0, mClientWidth, mClientHeight};
+}
